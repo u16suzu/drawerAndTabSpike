75529303720307d9677feb10439909abe9106f8d
u16suzu <u16suzu@gmail.com>
2015-02-24 19:19:50 +0900

    スワイプでドロワーの開閉をする｡

--- drawerSpike/SpikeDrawerController.m
+++ drawerSpike/SpikeDrawerController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.openDrawerGestureModeMask  = MMOpenDrawerGestureModeAll;    
+    self.closeDrawerGestureModeMask = MMCloseDrawerGestureModeAll;
+    
 }
 
 - (void)didReceiveMemoryWarning {
